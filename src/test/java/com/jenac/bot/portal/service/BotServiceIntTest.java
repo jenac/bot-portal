5d1aa76bca88f69c57c7d1a117eef98f32f3f4d8
Lihe Chen <lihe.chen@gmail.com>
2018-03-11 13:36:35 -0500

    wip

--- src/test/java/com/jenac/bot/portal/service/BotServiceIntTest.java
+++ src/test/java/com/jenac/bot/portal/service/BotServiceIntTest.java
@@ -1,9 +1,14 @@
 package com.jenac.bot.portal.service;
 
+import com.jenac.bot.portal.web.rest.vm.bot.ContactVM;
+import com.jenac.bot.portal.web.rest.vm.bot.ResponseVM;
+import com.jenac.bot.portal.web.rest.vm.bot.SendTextVM;
 import com.jenac.bot.portal.web.rest.vm.bot.StateVM;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.*;
 
@@ -17,9 +22,18 @@ public class BotServiceIntTest {
     }
 
     @Test
-    public void getState() {
+    public void testAllApi() {
+        //get state
         StateVM stateVM = botService.getState();
         assert (stateVM != null);
+        //find the test user
+        ContactVM found = stateVM.getContacts().stream().filter(c-> c.getRemark() == "test").findFirst().get();
+        assert (found != null);
+        //send text
+        SendTextVM sendTextVM = new SendTextVM();
+        sendTextVM.setTo(found.getUsername());
+        sendTextVM.setText("Hello, I am sending this message from spring unit test!");
+        ResponseVM responseVm = botService.sendText(sendTextVM);
     }
 
     @Test
